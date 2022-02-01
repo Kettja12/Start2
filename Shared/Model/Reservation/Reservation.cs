3597e677aba0331ffb50b4cd7452f558a3c93178
Jari Kettunen <jari.kettunen@protonmail.com>
2022-02-01 21:19:02 +0200

    *

---
+++ Shared/Model/Reservation/Reservation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Start2.Shared.Model.Reservation
+{
+    public class Reservation
+    {
+        public int Id { get; set; }
+        public int RoomId { get; set; }
+        public int CustomerId { get; set; }
+        public DateTime? ReservationDate { get; set; }
+    }
+
+}
---
+++ Shared/Model/ReservationFilter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Start2.Shared.Model
+{
+    public class ReservationFilter : Reservation.Reservation
+    {
+        public DateTime? ReservationDateStart { get; set; }
+        public DateTime? ReservationDateEnd { get; set; }
+    }
+
+}
